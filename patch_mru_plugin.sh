71b7bf9a6c58ce7db2e140befbf592156084be2c
Sindre Hansen <sindrehansen86@gmail.com>
2020-02-07 08:53:41 +0100

    Fix MRU patch script

--- patch_mru_plugin.sh
+++ patch_mru_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-cd ~/.vim_runtime/sources_non_forked/mru.vim/plugin/
+cd sources_non_forked/mru.vim/plugin/
 sed -i 's/botright/topleft/g' mru.vim
 echo "Patched the MRU plugin."
